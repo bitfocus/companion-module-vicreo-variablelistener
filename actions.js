0c7d98304b65e5923ba94d55e3223f7a58abf083
JeffreyDavidsz <jeffrey.davidsz@vicreo.eu>
2020-12-20 00:43:27 +0100

    upgrades

---
+++ actions.js
@@ -0,0 +1,7 @@
+exports.getActions = function () {
+
+	let actions = {}
+	actions['tallyOn'] = { label: 'Some action' },
+	actions['tallyOff']= { label: 'Some action1' }
+	return actions;
+};
--- index.js
+++ index.js
@@ -1,197 +1,266 @@
 const instance_skel = require('../../instance_skel');
+const actions = require('./actions');
 let debug;
 let log;
 
-function instance(system, id, config) {
-	var self = this;
-	// super-constructor
-	instance_skel.apply(this, arguments);
+class instance extends instance_skel {
 
-	return self;
-}
-
-instance.prototype.tallyOnListener = function (label, variable, value) {
-	const self = this;
-	const { tallyOnEnabled, tallyOnVariable, tallyOnValue, tallyOnEnabled2, tallyOnVariable2 } = self.config;
-
-	// Check for tally variable, if not enabled or other tally skip
-	if (!tallyOnEnabled || `${label}:${variable}` !== tallyOnVariable) {
-		return;
-	}
-
-	// if(tallyOnEnabled2) {
-	// 	//check for extra tally
-	// 	if(tallyOnVariable != tallyOnVariable2) {
-	// 		return;
-	// 	}
-	// }
-
-	// send tally info
-
-	self.system.emit('variable_parse', tallyOnValue, (parsedValue) => {
-		debug('variable changed... updating tally', { label, variable, value, parsedValue });
-		self.system.emit('action_run', {
-			action: (value === parsedValue ? 'tallyOn' : 'tallyOff'),
-			instance: self.id
+	constructor(system, id, config) {
+		super(system, id, config)
+		this.release_time = 200; // ms to send button release
+		this.value1;
+		this.value2;
+		Object.assign(this, {
+			...actions,
 		});
-	});
-}
 
-instance.prototype.setupEventListeners = function () {
-	const self = this;
-
-	if (self.config.tallyOnEnabled && self.config.tallyOnVariable) {
-		if (!self.activeTallyOnListener) {
-			self.activeTallyOnListener = self.tallyOnListener.bind(self);
-			self.system.on('variable_changed', self.activeTallyOnListener);
-		}
-	} else if (self.activeTallyOnListener) {
-		self.system.removeListener('variable_changed', self.activeTallyOnListener);
-		delete self.activeTallyOnListener;
+		this.actions()
 	}
-}
 
-instance.prototype.init = function () {
-	var self = this;
+	actions(system) {
+		this.setActions(this.getActions());
+	}
 
-	debug = self.debug;
-	log = self.log;
-
-	self.status(self.STATUS_WARNING, 'connecting');
-	self.actions(); // export actions
-	self.init_variables();
-	self.setInitialVariables();
-	self.setupEventListeners();
-}
-
-instance.prototype.setInitialVariables = function() {
-	var self = this;
-	self.setVariable('tallySource', "none");
-	self.setVariable('tallyOn', "off");
-}
-
-instance.prototype.updateConfig = function (config) {
-	var self = this;
-	self.config = config;
-	self.status(self.STATUS_UNKNOWN);
-	self.setupEventListeners();
-};
-
-// Return config fields for web config
-instance.prototype.config_fields = function () {
-	var self = this;
-
-	const dynamicVariableChoices = [];
-	self.system.emit('variable_get_definitions', (definitions) =>
-		Object.entries(definitions).forEach(([instanceLabel, variables]) =>
-			variables.forEach((variable) =>
-				dynamicVariableChoices.push({
-					id: `${instanceLabel}:${variable.name}`,
-					label: `${instanceLabel}:${variable.name}`
-				})
+	// Return config fields for instance config
+	config_fields() {
+		const dynamicVariableChoices = [];
+		this.system.emit('variable_get_definitions', (definitions) =>
+			Object.entries(definitions).forEach(([instanceLabel, variables]) =>
+				variables.forEach((variable) =>
+					dynamicVariableChoices.push({
+						id: `${instanceLabel}:${variable.name}`,
+						label: `${instanceLabel}:${variable.name}`
+					})
+				)
 			)
-		)
-	);
+		);
 
-	return [
-		{
-			type: 'text',
-			id: 'info',
-			width: 12,
-			label: 'Information',
-			value: 'This module controls checks variable for tally'
-		},
-		{
-			type: 'text',
-			id: 'tallyOnInfo',
-			width: 12,
-			label: 'Tally On',
-			value: 'Set tally ON when the variable from an other instance equals the value'
-		},
-		{
-			type: 'checkbox',
-			id: 'tallyOnEnabled',
-			width: 1,
-			label: 'Enable',
-			default: true
-		},
-		{
-			type: 'dropdown',
-			id: 'tallyOnVariable',
-			label: 'Tally On Variable',
-			width: 6,
-			tooltip: 'The instance label and variable name',
-			choices: dynamicVariableChoices,
-			minChoicesForSearch: 5
-		},
-		{
-			type: 'textinput',
-			id: 'tallyOnValue',
-			label: 'Tally On Value',
-			width: 5,
-			tooltip: 'When the variable equals this value, the camera tally light will be turned on.  Also supports dynamic variable references.  For example, $(atem:short_1)'
-		},
-		{
-			type: 'checkbox',
-			id: 'tallyOnEnabled2',
-			width: 1,
-			label: 'Enable comparison',
-			default: false
-		},
-		{
-			type: 'dropdown',
-			id: 'tallyOnVariable2',
-			label: 'Tally Variable to compare',
-			width: 6,
-			tooltip: 'The instance label and variable name',
-			choices: dynamicVariableChoices,
-			minChoicesForSearch: 5
+		return [
+			{
+				type: 'text',
+				id: 'info',
+				width: 12,
+				label: 'Information',
+				value: 'This module controls checks variable for tally'
+			},
+			{
+				type: 'text',
+				id: 'tallyOnInfo',
+				width: 12,
+				label: 'Tally On (Basic)',
+				value: 'Set tally ON when the variable from an other instance equals the value'
+			},
+			{
+				type: 'dropdown',
+				id: 'tallyOnVariable',
+				label: 'Tally On Variable',
+				width: 6,
+				tooltip: 'The instance label and variable name',
+				choices: dynamicVariableChoices,
+				minChoicesForSearch: 5
+			},
+			{
+				type: 'textinput',
+				id: 'tallyOnValue',
+				label: 'Tally On Value',
+				width: 6,
+				tooltip: 'When the variable equals this value, the camera tally light will be turned on.  Also supports dynamic variable references.  For example, $(atem:short_1)'
+			},
+			{
+				type: 'text',
+				id: 'tallyOnInfo',
+				width: 12,
+				label: 'Tally for shaders',
+				value: 'Set tally ON when the two variables matches'
+			},
+			{
+				type: 'checkbox',
+				id: 'tallyOnEnabled2',
+				width: 2,
+				label: 'Enable comparison',
+				default: false
+			},
+			{
+				type: 'dropdown',
+				id: 'tallyOnVariable2',
+				label: 'Tally Variable to compare',
+				width: 6,
+				tooltip: 'The instance label and variable name',
+				choices: dynamicVariableChoices,
+				minChoicesForSearch: 5
+			},
+			{
+				type: 'text',
+				id: 'tallyOnInfo',
+				width: 12,
+				label: 'Action',
+				value: 'When Tally is on press a button on the streamdeck'
+			},
+			{
+				type: 'checkbox',
+				id: 'buttonEnabled',
+				width: 2,
+				label: 'Enable press on button when tally',
+				default: false
+			},
+			{
+				type: 'textinput',
+				id: 'buttonBank',
+				label: 'Bank/Page',
+				width: 2
+			},
+			{
+				type: 'textinput',
+				id: 'buttonButton',
+				label: 'Button',
+				width: 2
+			},
+		]
+	};
+
+	tallyOnListener (label, variable, value) {
+		const { tallyOnVariable, tallyOnValue, tallyOnEnabled2, tallyOnVariable2, buttonBank, buttonButton } = this.config;
+		this.status(this.STATUS_OK);
+
+		if(tallyOnEnabled2) {
+			if (`${label}:${variable}` != tallyOnVariable) {
+				if (`${label}:${variable}` != tallyOnVariable2) {
+					return;
+				} else {
+					this.setVariable('tallySource2', value);
+					this.value2 = value;
+				}
+			} else {
+				this.setVariable('tallySource', value);
+				this.value1 = value;
+			}
+
+			if(this.value1 == this.value2) {
+				this.setVariable('tallyOn', 'On')
+				if(!!buttonBank && !!buttonButton && buttonEnabled) {
+					this.press_button(buttonBank, buttonButton)
+				}
+			} else {
+			setTimeout(() => {
+				this.setVariable('tallyOn', 'Off')
+			}, this.release_time);
+			}
+			return;
 		}
-	]
-};
 
-// When module gets deleted
-instance.prototype.destroy = function () {
-	var self = this;
-	if (self.activeTallyOnListener) {
-		self.system.removeListener('variable_changed', self.activeTallyOnListener);
-		delete self.activeTallyOnListener;
+		if (`${label}:${variable}` != tallyOnVariable) {
+			return;
+		}
+
+		this.setVariable('tallySource', value);
+		if (value == tallyOnValue) {
+			this.setVariable('tallyOn', 'On')
+				if(!!buttonBank && !!buttonButton && buttonEnabled) {
+					this.press_button(buttonBank, buttonButton)
+				}
+		} else {
+			setTimeout(() => {
+				this.setVariable('tallyOn', 'Off')
+			}, this.release_time);
+		}
+		
+		
+
+
+		// internal action
+		// this.system.emit('action_run', {
+		// 	action: (value === parsedValue ? 'tallyOn' : 'tallyOff'),
+		// 	instance: this.id
+		// });
+	}
+
+	setupEventListeners() {
+		if (this.activeTallyOnListener) {
+			this.system.removeListener('variable_changed', this.activeTallyOnListener);
+			delete this.activeTallyOnListener;
+		}
+		if (this.config.tallyOnVariable) {
+				this.activeTallyOnListener = this.tallyOnListener.bind(this);
+				this.system.on('variable_changed', this.activeTallyOnListener);
+		}
+	}
+
+	init() {
+		debug = this.debug;
+		log = this.log;
+
+		this.status(this.STATUS_UNKNOWN);
+		this.actions(); // export actions
+		this.init_variables();
+		this.setInitialVariables();
+		this.setupEventListeners();
+	}
+
+	setInitialVariables() {
+		const { tallyOnVariable, tallyOnVariable2 } = this.config;
+		if(!!tallyOnVariable){
+			let pos = tallyOnVariable.search(":");
+			this.system.emit('variable_get', tallyOnVariable.slice(0, pos), tallyOnVariable.slice(pos+1), (value) => {
+				this.setVariable('tallySource', value);
+			})
+		}
+		if(!!tallyOnVariable2){
+			let pos2 = tallyOnVariable2.search(":");
+			this.system.emit('variable_get', tallyOnVariable2.slice(0, pos2), tallyOnVariable2.slice(pos2+1), (value) => {
+				this.setVariable('tallySource2', value);
+			})
+		}
+		this.setVariable('tallyOn', "off");
+	}
+
+	updateConfig(config) {
+		this.config = config;
+		this.status(this.STATUS_UNKNOWN);
+		this.setInitialVariables();
+		this.setupEventListeners();
+	};
+
+	// When module gets deleted
+	destroy() {
+		if (this.activeTallyOnListener) {
+			this.system.removeListener('variable_changed', this.activeTallyOnListener);
+			delete this.activeTallyOnListener;
+		}
+	}
+
+	init_variables() {
+		var variables = [];
+		variables.push({ name: 'tallySource', label: 'Tally source' });
+		variables.push({ name: 'tallySource2', label: 'Tally source 2' });
+		variables.push({ name: 'tallyOn', label: 'Tally on' });
+		this.setVariableDefinitions(variables);
+	};
+
+	action(action) {
+		var opt = action.options;
+		var cmd = '';
+
+		switch (action.action) {
+
+			case 'tallyOn':
+				console.log("Tally ON");
+				break;
+
+			case 'tallyOff':
+				console.log("Tally OFF");
+				break;
+		}
+	};
+
+	press_button(bank, button) {
+		bank = parseInt(bank);
+		button = parseInt(button);
+
+		this.system.emit('bank_pressed', bank, button, true);
+
+		setTimeout(() => {
+			this.system.emit('bank_pressed', bank, button, false);
+		}, this.release_time);
 	}
 }
-
-instance.prototype.init_variables = function () {
-	var self = this;
-	var variables = [];
-	variables.push({ name: 'tallySource', label: 'Tally source' });
-	variables.push({ name: 'tallyOn', label: 'Tally on' });
-	self.setVariableDefinitions(variables);
-};
-
-instance.prototype.actions = function (system) {
-	var self = this;
-
-	self.system.emit('instance_actions', self.id, {
-		'tallyOn': { label: 'Some action' },
-		'tallyOff': { label: 'Some action1' }
-	});
-};
-
-instance.prototype.action = function (action) {
-	var self = this;
-	var opt = action.options;
-	var cmd = '';
-
-	switch (action.action) {
-
-		case 'tallyOn':
-			console.log("Tally ON");
-			break;
-
-		case 'tallyOff':
-			console.log("Tally OFF");
-			break;
-	}
-};
-
-instance_skel.extendedBy(instance);
 exports = module.exports = instance;
